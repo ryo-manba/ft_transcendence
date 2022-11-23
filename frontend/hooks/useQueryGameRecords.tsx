dd37ce08ec7c3d3c7f6eb803d484b2c9ed076614
yuta-fujimoto <fyuta@student.42tokyo.jp>
2022-11-23 16:23:20 +0900

    query error

--- frontend/hooks/useQueryGameRecords.tsx
+++ frontend/hooks/useQueryGameRecords.tsx
@@ -1,9 +1,10 @@
 import { useRouter } from 'next/router';
 import axios, { AxiosError } from 'axios';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { GameRecordWithUserName } from 'types/game';
 
 export const useQueryGameRecords = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   const getGameRecords = async () => {
     const { data } = await axios.get<GameRecordWithUserName[]>(
@@ -17,8 +18,16 @@ export const useQueryGameRecords = () => {
     queryKey: ['game'],
     queryFn: getGameRecords,
     onError: (err: AxiosError) => {
-      if (err.response?.status === 401 || err.response?.status === 403)
+      if (
+        err.response &&
+        (err.response.status === 401 || err.response.status === 403)
+      ) {
+        queryClient.removeQueries(['user']);
+        void axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL as string}/auth/logout`,
+        );
         void router.push('/');
+      }
     },
   });
 };
