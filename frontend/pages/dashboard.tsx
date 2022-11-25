8b4702c909c400423c86effd52f34ddd2d5339c5
Shogo Kohraku <skohraku@gmail.com>
2022-11-26 08:20:31 +0900

    fix: dashboardの画面上のログアウトボタンは不要のため削除

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -3,24 +3,10 @@ import Link from 'next/link';
 import { Stack, Button, Typography } from '@mui/material';
 import { useQueryUser } from 'hooks/useQueryUser';
 import { Header } from 'components/common/Header';
-import { signOut } from 'next-auth/react';
 import { Layout } from 'components/common/Layout';
-import axios from 'axios';
-import { LogoutIcon } from '@heroicons/react/solid';
-import { useQueryClient } from '@tanstack/react-query';
 
 const Dashboard: NextPage = () => {
   const { data: user } = useQueryUser();
-  const queryClient = useQueryClient();
-  const logout = async () => {
-    queryClient.removeQueries(['user']);
-    if (process.env.NEXT_PUBLIC_API_URL) {
-      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`);
-      void signOut({
-        callbackUrl: 'http://localhost:3000/',
-      });
-    }
-  };
 
   if (user === undefined) return <></>;
 
@@ -39,13 +25,6 @@ const Dashboard: NextPage = () => {
           <Button variant="contained">Friend</Button>
         </Link>
       </Stack>
-      <p>Sign out for mail login</p>
-      <LogoutIcon
-        className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
-        onClick={() => {
-          void logout();
-        }}
-      />
     </Layout>
   );
 };
