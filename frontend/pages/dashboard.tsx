f476407b438d515f496959dcbe030d748f610099
yuta-fujimoto <fyuta@student.42tokyo.jp>
2022-12-01 15:33:33 +0900

    clean dashboard

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,29 +1,135 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import { Stack, Button, Typography } from '@mui/material';
+import { Stack, Typography, Paper, Grid } from '@mui/material';
 import { useQueryUser } from 'hooks/useQueryUser';
 import { Header } from 'components/common/Header';
 import { Layout } from 'components/common/Layout';
 import { Loading } from 'components/common/Loading';
+import ChatIcon from '@mui/icons-material/Chat';
+import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import { useState } from 'react';
 
 const Dashboard: NextPage = () => {
   const { data: user } = useQueryUser();
+  const overElavation = 5;
+  const outElevation = 2;
+  const [chatShadow, setChatShadow] = useState(outElevation);
+  const [gameShadow, setGameShadow] = useState(outElevation);
+  const [friendShadow, setFriendShadow] = useState(outElevation);
 
-  if (user === undefined) return <Loading />;
+  if (user === undefined) return <Loading fullHeight />;
 
   return (
     <Layout title="Dashboard">
       <Header title="ft_transcendence" />
-      <Typography>HELLO {user.name}</Typography>
-      <Stack spacing={2} direction="row">
+      <Typography variant="h3" gutterBottom sx={{ margin: '50px' }}>
+        HELLO {user.name}
+      </Typography>
+      <Stack
+        spacing={6}
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+        sx={{ height: '100%', mt: '50px' }}
+      >
         <Link href="/chat">
-          <Button variant="contained">Chat</Button>
+          <Paper
+            sx={{
+              width: '200px',
+              height: '200px',
+            }}
+            elevation={chatShadow}
+            onMouseOver={() => {
+              setChatShadow(overElavation);
+            }}
+            onMouseOut={() => {
+              setChatShadow(outElevation);
+            }}
+          >
+            <Grid
+              container
+              spacing={1}
+              alignItems="center"
+              justifyContent="center"
+              sx={{
+                height: '100%',
+              }}
+            >
+              <Grid item>
+                <Typography fontWeight="fontWeightBold" variant="h5">
+                  Chat
+                </Typography>
+              </Grid>
+              <Grid item>
+                <ChatIcon />
+              </Grid>
+            </Grid>
+          </Paper>
         </Link>
         <Link href="/game/home">
-          <Button variant="contained">Game</Button>
+          <Paper
+            sx={{
+              width: '200px',
+              height: '200px',
+            }}
+            elevation={gameShadow}
+            onMouseOver={() => {
+              setGameShadow(overElavation);
+            }}
+            onMouseOut={() => {
+              setGameShadow(outElevation);
+            }}
+          >
+            <Grid
+              container
+              spacing={1}
+              alignItems="center"
+              justifyContent="center"
+              sx={{ height: '100%' }}
+            >
+              <Grid item>
+                <Typography fontWeight="fontWeightBold" variant="h5">
+                  Game
+                </Typography>
+              </Grid>
+              <Grid item>
+                <VideogameAssetIcon />
+              </Grid>
+            </Grid>
+          </Paper>
         </Link>
         <Link href="/friend">
-          <Button variant="contained">Friend</Button>
+          <Paper
+            sx={{
+              width: '200px',
+              height: '200px',
+            }}
+            elevation={friendShadow}
+            onMouseOver={() => {
+              setFriendShadow(overElavation);
+            }}
+            onMouseOut={() => {
+              setFriendShadow(outElevation);
+            }}
+          >
+            <Grid
+              container
+              spacing={1}
+              alignItems="center"
+              justifyContent="center"
+              sx={{ height: '100%' }}
+            >
+              <Grid item>
+                <Typography fontWeight="fontWeightBold" variant="h5">
+                  Friend
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Diversity3Icon />
+              </Grid>
+            </Grid>
+          </Paper>
         </Link>
       </Stack>
     </Layout>
