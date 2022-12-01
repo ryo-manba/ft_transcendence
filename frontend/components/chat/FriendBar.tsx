da9281fdbb81b673cf85bcfcd1df4cbf71c79e1e
Ryo <matsukawa.ryo42@gmail.com>
2022-12-01 14:53:31 +0900

    add: fetchFollowingUser

--- frontend/components/chat/FriendBar.tsx
+++ frontend/components/chat/FriendBar.tsx
@@ -1,18 +1,14 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
 import { List } from '@mui/material';
 // import { FriendAddButton } from 'components/chat/FriendAddButton';
 import { FriendListItem } from 'components/chat/FriendListItem';
 import { Loading } from 'components/common/Loading';
 import { useQueryUser } from 'hooks/useQueryUser';
 import { Friend } from 'types/friend';
+import { fetchFollowingUser } from 'api/friend/fetchFollowingUser';
 
 export const FriendBar = () => {
   const [friends, setFriends] = useState<Friend[]>([]); // fetchする
-  const endpoint = `${
-    process.env.NEXT_PUBLIC_API_URL as string
-  }/friends/followings`;
-
   const { data: user } = useQueryUser();
   if (user === undefined) {
     return <Loading />;
@@ -20,11 +16,9 @@ export const FriendBar = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await axios.get<Friend[]>(endpoint, {
-        params: { id: user.id },
-      });
+      const res = await fetchFollowingUser({ userId: user.id });
 
-      setFriends(res.data);
+      setFriends(res);
     };
 
     fetchData()
