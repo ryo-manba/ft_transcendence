356e06776897e20ca2ad9b5cbf9a1527b9050a38
yuta-fujimoto <fyuta@student.42tokyo.jp>
2022-12-02 13:22:48 +0900

    set loading page

--- frontend/components/common/Layout.tsx
+++ frontend/components/common/Layout.tsx
@@ -1,5 +1,8 @@
+import { useQueryUser } from 'hooks/useQueryUser';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { FC, ReactNode } from 'react';
+import { Loading } from './Loading';
 
 type Props = {
   title: string;
@@ -14,6 +17,12 @@ export const Layout: FC<Props> = ({
   divClassName = '',
   mainClassName = '',
 }) => {
+  const router = useRouter();
+  if (router.pathname !== '/') {
+    const { isSuccess } = useQueryUser();
+    if (!isSuccess) return <Loading fullHeight />;
+  }
+
   return (
     <div className={divClassName}>
       <Head>
