192551b725e5ec1f9be9fe32ab0b23dd48d6f898
takashi247 <takashi247@gmail.com>
2022-12-06 21:55:07 +0900

    simplify error handling in useQueryGameRecords

--- frontend/hooks/useQueryGameRecords.tsx
+++ frontend/hooks/useQueryGameRecords.tsx
@@ -1,11 +1,8 @@
-import { useRouter } from 'next/router';
 import axios, { AxiosError } from 'axios';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { GameRecordWithUserName } from 'types/game';
 
 export const useQueryGameRecords = (userId: number | undefined) => {
-  const queryClient = useQueryClient();
-  const router = useRouter();
   const getGameRecords = async () => {
     if (userId === undefined) return undefined;
     const { data } = await axios.get<GameRecordWithUserName[]>(
@@ -19,16 +16,8 @@ export const useQueryGameRecords = (userId: number | undefined) => {
     queryKey: ['game', userId],
     queryFn: getGameRecords,
     onError: (err: AxiosError) => {
-      if (
-        err.response &&
-        (err.response.status === 401 || err.response.status === 403)
-      ) {
-        queryClient.removeQueries(['user']);
-        void axios.post(
-          `${process.env.NEXT_PUBLIC_API_URL as string}/auth/logout`,
-        );
-        void router.push('/');
-      }
+      console.log(err);
+      throw err;
     },
   });
 };
