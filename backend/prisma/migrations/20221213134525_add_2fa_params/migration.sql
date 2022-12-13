e9445b3feb30ccc93accb9a7af9da33352a22a57
Shogo Kohraku <skohraku@gmail.com>
2022-12-13 23:04:43 +0900

    fix: migrationファイルの再作成

--- backend/prisma/migrations/20221213134525_add_2fa_params/migration.sql
+++ backend/prisma/migrations/20221213134525_add_2fa_params/migration.sql
@@ -1,15 +1,11 @@
 /*
   Warnings:
 
-  - You are about to drop the column `oauthid` on the `User` table. All the data in the column will be lost.
   - A unique constraint covering the columns `[oAuthId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
 
 */
--- DropIndex
-DROP INDEX "User_oauthid_key";
-
 -- AlterTable
-ALTER TABLE "User" DROP COLUMN "oauthid",
+ALTER TABLE "User" ADD COLUMN     "has2FA" BOOLEAN NOT NULL DEFAULT false,
 ADD COLUMN     "oAuthId" TEXT,
 ADD COLUMN     "secret2FA" TEXT NOT NULL DEFAULT '';
 
