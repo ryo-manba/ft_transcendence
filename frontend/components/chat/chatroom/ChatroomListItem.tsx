9e21597eb3c945413bd27926c88fa47bdd169e7b
Ryo <matsukawa.ryo42@gmail.com>
2023-01-08 18:34:44 +0900

    update: Wider clickable area in chatroom

--- frontend/components/chat/chatroom/ChatroomListItem.tsx
+++ frontend/components/chat/chatroom/ChatroomListItem.tsx
@@ -270,6 +270,9 @@ export const ChatroomListItem = memo(function ChatroomListItem({
         }
         divider
         button
+        onClick={() => {
+          changeCurrentRoom(room.id);
+        }}
       >
         <ListItemAvatar>
           {room.type === ChatroomType.DM ? (
@@ -295,9 +298,6 @@ export const ChatroomListItem = memo(function ChatroomListItem({
         />
         <ListItemText
           primary={room.name}
-          onClick={() => {
-            changeCurrentRoom(room.id);
-          }}
           style={{
             overflow: 'hidden',
           }}
