d21d16433c5015aab32729730c30ae6b9f7f930a
takashi247 <takashi247@gmail.com>
2023-01-29 12:56:13 +0900

    add: avatar image to chat message

--- frontend/components/chat/message-exchange/ChatMessage.tsx
+++ frontend/components/chat/message-exchange/ChatMessage.tsx
@@ -1,9 +1,11 @@
 import { Avatar } from '@mui/material';
+import { getAvatarImageUrl } from 'api/user/getAvatarImageUrl';
 
 type Props = {
   message: string;
   timestamp: Date;
   displayName: string;
+  userId: number;
 };
 
 /**
@@ -22,48 +24,21 @@ const truncateDate = (date: Date): string => {
   return res;
 };
 
-/**
- * DOCS: https://mui.com/material-ui/react-avatar/#letter-avatars
- * 名前によって背景色を変更する
- */
-const stringToColor = (string: string) => {
-  let hash = 0;
-  let i;
-
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-
-  return color;
-};
-
-const stringAvatar = (name: string) => {
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: name[0],
-  };
-};
-
-export const MessageLeft = ({ message, timestamp, displayName }: Props) => {
+export const MessageLeft = ({
+  message,
+  timestamp,
+  displayName,
+  userId,
+}: Props) => {
   // 時間を表示用に変換する
   const displayTimestamp = truncateDate(new Date(timestamp));
+
+  const avatarImageUrl = getAvatarImageUrl(userId);
 
   return (
     <>
       <div style={{ display: 'flex', margin: '10px' }}>
-        <Avatar
-          // eslint-disable-next-line react/jsx-props-no-spreading
-          {...stringAvatar(displayName)}
-        />
+        <Avatar src={avatarImageUrl} />
         <div>
           <div
             style={{
