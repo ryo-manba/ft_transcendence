157baae8fb4aa053dbd60c54b4d4dc3f978512d2
takashi247 <takashi247@gmail.com>
2023-01-31 14:28:57 +0900

    fix: import a required type in user.ts

--- frontend/types/user.ts
+++ frontend/types/user.ts
@@ -1,3 +1,5 @@
+import { UserStatus } from 'types/game';
+
 // frontendで受け取るUser型は、hashedPasswordなど不要なものを取り除いたClientUser型として扱う。
 // backend/src/user/types/user.tsと型を合わせること
 export type ClientUser = {
